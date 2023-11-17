fbae4a991ec98072c8cfa0f66c79cf252f72edcf
문정우 <106897607+MoonjeongWoo@users.noreply.github.com>
2023-11-17 21:20:09 +0900

    Revert "make come inside buttons and animation"
    This reverts commit 3bd7431e982ae4731b8f374f5b036b2d4871a837.

--- resume/src/app/navbar/page.tsx
+++ resume/src/app/navbar/page.tsx
@@ -1,25 +1,10 @@
-import Link from "next/link";
-
-const Navbar = () => {
-    return (
-        <nav className="bg-neutral-50 shadow-md">
-            <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-                <div className="flex items-center">
-                    <Link href="/">
-                        <span className="text-gray-800 text-xl font-semibold hover:text-gray-600 cursor-pointer">Home</span>
-                    </Link>
-                </div>
-                <div className="flex items-center space-x-4">
-                    <Link href="/">
-                        <span className="text-gray-800 hover:text-gray-600 cursor-pointer">Intro</span>
-                    </Link>
-                    <Link href="/About">
-                        <span className="text-gray-800 hover:text-gray-600 cursor-pointer">About me</span>
-                    </Link>
-                </div>
-            </div>
-        </nav>
-    );
+import Link from "next/link"
+export default function Navbar(){
+    return(
+        <div>
+            <Link href="/">Home</Link>
+            <Link href="/">Intro</Link>
+            <Link href="/About">About me</Link>
+        </div>
+    )
 }
-
-export default Navbar;
