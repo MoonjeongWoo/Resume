73141bd51425c0dc56d2ea393fca0a5ed348666f
문정우 <106897607+MoonjeongWoo@users.noreply.github.com>
2023-12-07 20:53:12 +0900

    Update profile.tsx

--- resume/src/app/components/spec/profile.tsx
+++ resume/src/app/components/spec/profile.tsx
@@ -1,11 +1,22 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 const Profile = () => {
+  const [isupted, setIsUpted] = useState(false);
   return (
     <>
-      <div className="flex bg-white rounded-xl h-20 text-center items-center justify-center cursor-pointer">
-        <div className="">여기는 프로파일 이에요</div>
+      <div
+        onClick={() => {
+          setIsUpted(!isupted);
+          console.log("isupted", isupted);
+        }}
+        className="flex bg-white rounded-xl h-20 text-center items-center justify-center cursor-pointer"
+      >
+        {isupted ? (
+          <div className="">안녕하세요</div>
+        ) : (
+          <div className="">안녕하시와요!</div>
+        )}
       </div>
     </>
   );
