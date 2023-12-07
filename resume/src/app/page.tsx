4acf805ba2b1b4bff641ce1bb95ff0b14215a8f7
문정우 <106897607+MoonjeongWoo@users.noreply.github.com>
2023-12-07 21:45:32 +0900

    Revert "Next 누르면 Aboutme 로 된다"
    This reverts commit 6f1d2ce53b0589e9ec5234c78319198463f48987.

--- resume/src/app/page.tsx
+++ resume/src/app/page.tsx
@@ -1,11 +1,7 @@
-"use client";
 import React from "react";
 import Imagecomp from "./components/Image/Imagecomp";
 import Intro from "./components/Intro/Intro";
-import useStateStore from "./zustand/statements";
-import Aboutme from "./aboutme/page";
 export default function Home() {
-  const { isClicked, setIsClicked } = useStateStore();
   return (
     <main>
       <div className="flex flex-col items-center justify-center">
@@ -13,7 +9,7 @@ export default function Home() {
           <Imagecomp />
         </div>
         <div className="m-10 w-1/3">
-          {isClicked > 4 ? <Aboutme></Aboutme> : <Intro></Intro>}
+          <Intro></Intro>
         </div>
       </div>
     </main>
